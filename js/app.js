b91216f8be02721ab10c3e3622282a241d0fb7a5
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-12 14:32:13 -0400

    add loop over board array

--- js/app.js
+++ js/app.js
@@ -3,14 +3,11 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let currentPlayer = 1 
+let currentPlayer 
 const winMessage = `Player ${currentPlayer} has won!`
 const tieMessage = `Game ended in a draw!`
 const currentPlayerMessage = `It's ${currentPlayer}'s turn`
-let gameBoard = [
-  null, null, null, 
-  null, null, null, 
-  null, null, null]
+let gameBoard = []
 let isWinner 
 /*------------------------ Cached Element References ------------------------*/
 const sq0 = document.getElementById('sq0')
@@ -23,6 +20,8 @@ const sq6 = document.getElementById('sq6')
 const sq7 = document.getElementById('sq7')
 const sq8 = document.getElementById('sq8')
 const gameStatus = document.getElementById(message)
+
+let square = [sq0, sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8]
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -32,10 +31,21 @@ init()
 
 function init() {
   gameBoard = [
-    sq0, sq1, sq2, 
-    sq3, sq4, sq5, 
-    sq6, sq7, sq8]
+  null, null, null, 
+  null, null, null, 
+  null, null, null]
   currentPlayer = 1
   isWinner = null
   render()
+}
+function render() {
+  gameBoard.forEach(function(box,idx) {
+    box = square[idx]
+    if (currentPlayer === 1) {
+      box.style.backgroundColor = '#809bce'
+    } else if (currentPlayer === -1) {
+      box.style.backgroundColor = '#c1bbde'
+    } 
+  })
+  
 }
